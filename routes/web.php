fbf44c5e94940c2cc95715e8b5fd559eeebb922d
2388426660 <2388426660@qq.com>
2018-12-27 20:55:37 +0800

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+// 静态界面
 Route::get('/', 'StaticPagesController@home')->name('static.home');
 Route::get('/help', 'StaticPagesController@help')->name('static.help');
 Route::get('/about', 'StaticPagesController@about')->name('static.about');
+
+Route::get('/signup', 'UsersController@store')->name('signup');
