23d4f8736715dfc189ea3ad3e23c491a3b318779
2388426660 <2388426660@qq.com>
2019-01-01 20:01:02 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,7 +35,6 @@ class User extends Authenticatable
            $user->activation_token = str_random(30);
         });
     }
-
     // 用户有很多动态
     public function statuses()
     {
@@ -46,5 +45,41 @@ class User extends Authenticatable
     {
         return $this->statuses()
                     ->orderBy('created_at', 'desc');
+    }
+
+    // 粉丝
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    // 关注的人
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    // 关注
+    public function follow($user_ids)
+    {
+        if (! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    // 取关
+    public function unfollow($user_ids)
+    {
+        if (! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    // 判断是否关注
+    public function isFollowing($user_id)
+    {
+        return $this->followings()->contains($user_id);
     }
 }
